5399932c8c8f3be36e294342c888f449167d9eef
VesPet <Kaktus.1@seznam.cz>
2025-04-07 10:42:01 +0200

    update files: README, Config_dev, loginClass, globalTypes,feTest.spec

--- src/pom/loginClass.ts
+++ src/pom/loginClass.ts
@@ -3,19 +3,23 @@ import { TestConfig, TestSecrets } from "../types/globalTypes";
 import { FeUtils } from "../lib/frontend";
 
 
+
 export class LoginPage extends FeUtils {
     protected _testConfig: TestConfig
     protected _testSecrets: TestSecrets
-
+    
+       
     constructor(
         page: Page,
         test: TestType<PlaywrightTestArgs & PlaywrightTestOptions, PlaywrightWorkerArgs & PlaywrightWorkerOptions>,
         testConfig: TestConfig,
-        testSecrets: TestSecrets
+        testSecrets: TestSecrets 
+                    
     ) {
         super(page, test)
         this._testConfig = testConfig
         this._testSecrets = testSecrets
+       
     }
 
     //metody
@@ -23,6 +27,27 @@ export class LoginPage extends FeUtils {
         await this._goTo(this._testConfig.endpointUrl)
         await expect(this._page).toHaveURL(this._testConfig.endpointUrl)
     }
+    
+    public async productStoreButtonIsVisible(){
+       await expect (this._page.locator('a#nava')).toBeVisible()
+    }
+
+    public async productStoreButtonContainText(){
+        await expect (this._page.locator('a#nava')).toContainText('PRODUCT')
+     }
+
+    public async loginButtonIsVisible(){
+        await expect (this._page.locator('#login2')).toBeVisible()
+     }
+ 
+    public async loginButtonContainText(){
+         await expect (this._page.locator('#login2')).toHaveText('Log in')
+    }
+    public async loginButtonClick (){
+        await this._clickBySelector('#login2')
+     }
+ 
+    
 
 }
  
--- tests/feTest.spec.ts
+++ tests/feTest.spec.ts
@@ -1,20 +1,53 @@
-import { test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 import { LoginPage } from '../src/pom/loginClass'
 import dotenv from 'dotenv'
 import { TestConfig, TestSecrets } from '../src/types/globalTypes'
 
-dotenv.config({ override: true })
+test.describe("Test Suite", () => {
 
-const env = process.env.ENV || 'dev'
-const testConfig: TestConfig = require(`../data/envs/config_${env}.json`)
+    dotenv.config({ override: true })
 
-const testSecrets: TestSecrets = {
-    username: process.env.UNAME,
-    password: process.env.PWORD
-}
+    const env = process.env.ENV || 'dev'
+    const testConfig: TestConfig = require(`../data/envs/config_${env}.json`)
 
-test("Sample Frontend", async({ page }) => {
-    const loginPage = new LoginPage(page, test, testConfig, testSecrets)
+    const testSecrets: TestSecrets = {
+        username: process.env.UNAME,
+        password: process.env.PWORD        
+    }
+    test("Header section tests", async({ page }) => {      
 
-    await loginPage.openHomepage()
+        await test.step ("Otevřít stránku www.demoblaze.com", async () => {
+            const loginPage = new LoginPage(page, test, testConfig, testSecrets)
+            await loginPage.openHomepage();
+        })
+
+        await test.step ("Home buttton/logo", async() =>{
+            const homeButtonSelector = 'a#nava'            
+            await expect (page.locator(homeButtonSelector)).toBeVisible()
+            await expect (page.locator(homeButtonSelector)).toContainText('PRODUCT STORE')
+            await expect (page.locator(homeButtonSelector)).toHaveAttribute('href')
+        })
+       
+        await test.step ("Ovládací prvky záhlaví - tlačítko 'Log in'", async() =>{
+            const loginMenuSelector = '#login2'                  
+            await expect (page.locator(loginMenuSelector)).toBeVisible()
+            await expect (page.locator(loginMenuSelector)).toContainText('Log in')           
+        })
+
+        await test.step ("Akce přihlásit se včetně vyplnění jména a hesla'", async() =>{
+            const loginMenuSelector = '#login2'
+            const modalSelector = '#logInModal'
+            const usernameSelector = '#loginusername'  
+            const passwordModal = "#loginpassword"
+            const loginButtonSelector = 'button.btn.btn-primary'
+            const nameOfUserSelector = 'a#nameofuser'                                                         
+            await (page.locator(loginMenuSelector)).click()
+            await expect (page.locator(modalSelector)).toBeVisible()
+            await (page.locator(usernameSelector)).fill(`Postolka`,{timeout: 3000})
+            await (page.locator(passwordModal)).fill('Letidomu',{timeout: 3000})
+            await (page.locator(loginButtonSelector)).nth(2).click()
+            await expect (page.locator(nameOfUserSelector)).toContainText('Welcome ')
+        })        
+    })
 })
+    
